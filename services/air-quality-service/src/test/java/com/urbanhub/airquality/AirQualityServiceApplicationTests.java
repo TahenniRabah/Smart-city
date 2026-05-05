bcbfa22a46639154c060c228e785f6381a231187
TAHENNI <rabah.tahenni@capgemini.com>
2026-05-05 20:36:49 +0200

    adding airQuality Service

---
+++ services/air-quality-service/src/test/java/com/urbanhub/airquality/AirQualityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanhub.airquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
