1f00a75134125e4a7d8ed892a9ee1c4c2213074e
TAHENNI <rabah.tahenni@capgemini.com>
2026-05-05 22:02:05 +0200

    refactor: extract NO2 threshold constants

--- services/air-quality-service/src/main/java/com/urbanhub/airquality/Application/AirQualityAlertService.java
+++ services/air-quality-service/src/main/java/com/urbanhub/airquality/Application/AirQualityAlertService.java
@@ -7,17 +7,30 @@ import com.urbanhub.airquality.Domain.Pollutant;
 
 public class AirQualityAlertService {
 
+    private static final double NO2_WARNING_THRESHOLD = 100;
+    private static final double NO2_CRITICAL_THRESHOLD = 200;
+
     public AlertLevel calculateAlertLevel(AirQualityMeasurement measurement) {
 
-        if (measurement.pollutant() == Pollutant.NO2 && measurement.value() >= 200) {
+        if (measurement.pollutant() == Pollutant.NO2) {
+            return calculateNo2Level(measurement.value());
+        }
+
+        return AlertLevel.NORMAL;
+    }
+
+
+    private AlertLevel calculateNo2Level(double value) {
+
+        if (value >= NO2_CRITICAL_THRESHOLD) {
             return AlertLevel.CRITICAL;
         }
 
-
-        if (measurement.pollutant() == Pollutant.NO2 && measurement.value() >= 100) {
+        if (value >= NO2_WARNING_THRESHOLD) {
             return AlertLevel.WARNING;
         }
 
         return AlertLevel.NORMAL;
     }
+
 }
