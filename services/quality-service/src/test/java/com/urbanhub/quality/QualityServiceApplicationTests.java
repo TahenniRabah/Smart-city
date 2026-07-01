383e3c3dd6e57ad8857ea860c6d9e215a33bbdc4
TAHENNI <rabah.tahenni@capgemini.com>
2026-07-01 15:11:50 +0200

    chore: create quality service

---
+++ services/quality-service/src/test/java/com/urbanhub/quality/QualityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanhub.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class QualityServiceApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
